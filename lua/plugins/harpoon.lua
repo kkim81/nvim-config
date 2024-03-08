02b2f4d55d349ae05475a80ecf7a0aa6f4d59fa5
kkim81 <kylekim81@gmail.com>
2024-03-07 23:48:09 -0500

    harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -23,15 +23,15 @@ return {
     end
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-a>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    vim.keymap.set("n", "<C-S-Q>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-W>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<C-H>", function() toggle_telescope(harpoon:list()) end,
       { desc = "Open harpoon window" })
   end
 }
